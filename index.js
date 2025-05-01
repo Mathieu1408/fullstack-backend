ff7831bfae41061835377ae9aee62d2774e7c6ea
Mathieu1408 <mtran20@myges.fr>
2025-05-01 13:10:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
 const express = require('express');
-const { getProduits } = require('./db');
 const app = express();
 
-// Route de test
 app.get('/', (req, res) => {
   res.send('Backend API is working on Azure!');
 });
 
-app.get('/api/produits', async (req, res) => {
-  try {
-    const produits = await getProduits();
-    res.json(produits);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Erreur lors de la récupération des produits');
-  }
-});
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Serveur démarré sur le port ${port}`);
+  console.log(`Serveur backend démarré sur le port ${port}`);
 });
